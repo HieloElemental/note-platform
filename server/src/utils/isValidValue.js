9a1ddcc0798c7e8d8bb22619eff3564803569058
HieloElemental <hieloelemental@gmail.com>
2023-06-23 16:55:22 -0500

    correction to isValidString on isValidValue

--- server/src/utils/isValidValue.js
+++ server/src/utils/isValidValue.js
@@ -1,4 +1,7 @@
 const isValidString = (str) => {
+  if (!str || typeof str !== "string") {
+    return false;
+  }
   const normalCharactersPattern = /^[a-zA-Z0-9\sñÑ]+$/;
   return normalCharactersPattern.test(str);
 };
