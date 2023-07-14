795426af6e298014d54bce79408223684403bfbd
HieloElemental <hieloelemental@gmail.com>
2023-07-14 17:20:14 -0500

    Client: fixed main route

--- client/src/routes/Home/index.jsx
+++ client/src/routes/Home/index.jsx
@@ -1,17 +1,21 @@
 import RequireAuth from "./../../auth/RequireAuth";
 import Navbar from "./../../components/Navbar";
 
+import "./index.css";
+
 const Home = () => {
   return (
-    <div>
-      <Navbar />
-      <p>
-        This is the home page{" "}
-        <RequireAuth allowedRoles={["teacher"]}>teacher user</RequireAuth>
-        <RequireAuth allowedRoles={["admin"]}>admin user</RequireAuth>
-        <RequireAuth allowedRoles={["student"]}>student user</RequireAuth>
-      </p>
-    </div>
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <p>
+          This is the homepage{" "}
+          <RequireAuth allowedRoles={["admin"]}>admin user</RequireAuth>
+        </p>
+      </main>
+    </>
   );
 };
 
