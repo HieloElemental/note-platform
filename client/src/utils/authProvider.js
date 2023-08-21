8d5af60e7eddbaa44cad9c7d7b6a707ed86d0433
HieloElemental <hieloelemental@gmail.com>
2023-08-20 21:01:29 -0500

    Client: add .env system

--- client/src/utils/authProvider.js
+++ client/src/utils/authProvider.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://192.168.1.6:3000";
+const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 const signIn = async ({ userUsername, userPassword }) => {
   try {
